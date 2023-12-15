fa32c6245d3bf41ed0159b0596896f5d797c5968
Lazarte, Hector <hector.lazarte@nearsure.net>
2023-12-15 17:52:13 -0400

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+
+namespace Temperatura
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DateTime Lunes = DateTime.Today;
+      String connStr = "User ID=INFOMOVIL;Password=INFO";
+      string mensaje = "";
+      SerialPort puerto;
+
+      String filePath = AppDomain.CurrentDomain.BaseDirectory + "//Log" + DateTime.Today.ToString("yyyy-MM-dd") + ".log";
+      TextWriter writer = File.CreateText(filePath);
+
+      //Busca localización de los archivos
+      FachadaINFOMOVIL.Servicios servInfo = new FachadaINFOMOVIL.Servicios();
+
+      puerto = new SerialPort
+      {
+        PortName = "COM3",
+        BaudRate = 9600,
+        ReadTimeout = 5000
+      };
+
+      try
+      {
+        if (!puerto.IsOpen) puerto.Open();
+        String cadena = puerto.ReadLine();
+        puerto.Close();
+        FachadaINFOMOVIL.Medicion Medicion = new FachadaINFOMOVIL.Medicion();
+        Medicion.FECHA = DateTime.Now;
+        Medicion.LECTURA = double.Parse(cadena);
+        servInfo.GrabaMedicion(Medicion, connStr, ref mensaje);
+        if (mensaje != "") throw new Exception(mensaje);
+        log(writer, "LECTURA(" + DateTime.Now.ToShortTimeString() + ") " + cadena);
+      }
+      catch (Exception ex)
+      {
+        FachadaINFOMOVIL.Medicion Medicion = new FachadaINFOMOVIL.Medicion();
+        Medicion.FECHA = DateTime.Now;
+        Medicion.DESCRIPCION = "Error :" + ex.Message;
+        servInfo.GrabaMedicion(Medicion, connStr, ref mensaje);
+        if (puerto.IsOpen) puerto.Close();
+        log(writer, ex.Message);
+      }
+
+      writer.Close();
+    }
+    static void log(TextWriter writer, String text)
+    {
+      Console.OutputEncoding = System.Text.Encoding.UTF8;
+      writer.WriteLine(text);
+      Console.WriteLine(text);
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Back End de Infomóvil
+
+# Arquitectura
+
+![Arquitectura](/medios/arquitectura.png "Arquitectura")
+
+* **Base de Datos.-** Como RDBMS podemos utilizar ORACLE
+
+* **Entidades de Negocio.-** Se prevé el mapeo a objetos ORM, estas se constituyen en
+las únicas entidades comunes a todas las capas. Solo contienen datos y son
+mapeadas a entidades proxy en la capa de presentación.
+
+* **Acceso a Datos.-** Esta capa se encarga de la persistencia de la información, traduce
+los cambios en las entidades de negocios a comandos de manipulación de las
+bases de datos y/o archivos y/o almacenamiento web.
+
+* **Capa de negocio.-** Aquí se programa las reglas de negocio en general, por ejemplo
+el nivel de acceso a las funciones según usuarios y perfiles. Aquí se codifican los
+mecanismos de interacción entre los usuarios, niveles y sistema.
+
+* **Servicios.-** También llamada capa de transporte transforma los objetos y métodos
+en funciones web visibles y utilizables en Internet.
